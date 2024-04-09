4150a6ccebbe213f9bc22025301d107aab10e4d1
jang hyun cho <janghyuncho7@utexas.edu>
2024-04-08 21:14:16 -0500

    website

--- decola/modeling/decola_deta/decola_deta_config.py
+++
@@ -1,8 +0,0 @@
-from detectron2.config import CfgNode as CN
-
-def add_decola_deta_config(cfg):
-    _C = cfg
-    _C.MODEL.DECOLA.DETA = CN()
-    _C.MODEL.DECOLA.DETA.USE_DETA = False  
-    _C.MODEL.DECOLA.DETA.ASSIGN_FIRST_STAGE = True 
-    _C.MODEL.DECOLA.DETA.ASSIGN_SECOND_STAGE = True 
